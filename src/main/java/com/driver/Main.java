c6b81cde567bdab507bd64f9826101ceb900ca59
parag1627 <paragpatil973047@gmail.com>
2023-02-09 13:30:50 +0530

    added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,8 @@
 package com.driver;
 
 public class Main {
-  
-}
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("John");
+        System.out.println("Name: " + obj.getName());
+}}
